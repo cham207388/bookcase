06dccf64dca03a603962c3105fed4df4b39f4be7
Alhagie Bai Cham <alhagiebcham.ee@gmail.com>
2019-09-22 15:00:44 -0400

    clean up

--- src/main/java/com/abc/entity/Author.java
+++ src/main/java/com/abc/entity/Author.java
@@ -35,19 +35,19 @@ public class Author {
     @Column(name = "last_name")
     @NotNull(message = "Last Name is required")
     @Size(min = 3, message = "Last name must be at least 3 characters")
-    @ApiModelProperty(notes = "last name must be at least 3 characters")
+    @ApiModelProperty(notes = "Last name must be at least 3 characters")
     private String lastName;
 
     @Column(name = "email", unique = true)
     @NotNull(message = "email cannot be null")
     @Email
     @Pattern(regexp = "[A-Za-z0-9._%-+]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}")
-    @ApiModelProperty(notes = "email must follow proper format")
+    @ApiModelProperty(notes = "Email must follow proper format")
     private String email;
 
     @NotNull(message = "Password")
     @Size(min = 8, message = "Password must be >= 8")
-    @ApiModelProperty(notes = "password must be at least 8 characters")
+    @ApiModelProperty(notes = "Password must be at least 8 characters")
     private String password;
 
     @OneToMany(cascade = CascadeType.ALL,
