067b4521a90f4b44c8efbf710ceba0fb03cfe9b9
Alhagie Bai Cham <alhagiebcham.ee@gmail.com>
2019-09-26 21:53:02 -0400

    edited the swagger file

--- src/main/java/com/abc/config/SwaggerConfig.java
+++ src/main/java/com/abc/config/SwaggerConfig.java
@@ -1,18 +1,10 @@
 package com.abc.config;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
-
+import com.abc.SpringApplicationContext;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-
-import com.abc.SpringApplicationContext;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -20,32 +12,11 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-/*
 
-@Controller
-@EnableSwagger2
-public class SwaggerConfig {
-	public static final Contact DEFAULT_CONTACT = new Contact("Alhagie Bai Cham", "website", "cham207388@gmail.com");
-	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Bookcase API", "Electronic bookcase", "1.0",
-			"urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
-	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(
-			Arrays.asList("application/json", "application/xml"));
-
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
-				.consumes(DEFAULT_PRODUCES_AND_CONSUMES)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build()
-				.apiInfo(DEFAULT_API_INFO);
-	}
-	
-
-}*/
-
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Configuration
 @EnableSwagger2
